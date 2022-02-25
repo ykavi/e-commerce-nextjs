9c9e647b7e93d02bb32eeb01d7d69b26750d67c5
Yunus Emre Kavi <ykavi@ebay.com>
2022-02-26 00:48:11 +0300

    small editing

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import {Grid, Col, Row} from 'react-styled-flexboxgrid'
+import { Grid, Col, Row } from 'react-styled-flexboxgrid';
 
-const Home = () =>
-    <Grid>
-        <Row>
-            <Col lg={6}>
-                <p>Kavi</p>
-            </Col>
-            <Col lg={6}>
-                <p>Kavi</p>
-            </Col>
-        </Row>
-    </Grid>;
+const Home = () => (
+  <Grid>
+    <Row>
+      <Col lg={6}>
+        <p>Kavi</p>
+      </Col>
+      <Col lg={6}>
+        <p>Kavi</p>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 export default Home;
