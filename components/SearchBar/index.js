0f55851e60b83509c03f8a65f12e74ae4f2ab3b3
Kavi <ykavi@ebay.com>
2022-03-09 00:31:34 +0300

    added svgr cli

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,9 +1,11 @@
 import { Grid, Col, Row } from 'react-styled-flexboxgrid';
 import { StyledInput, Container } from './style';
+import { Search } from '@Icons';
 
 const SearchBar = () => (
   <Container>
     <StyledInput type="text" />
+    <Search />
   </Container>
 );
 
