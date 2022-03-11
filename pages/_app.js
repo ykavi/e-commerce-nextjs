1740755bf5e520ee4e51420441a6d92df89631bc
Kavi <ykavi@ebay.com>
2022-03-11 15:34:33 +0300

    revert last commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>SHOP</title>;
       <meta name="description" content={'description'} />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
     </Head>
 
     <ThemeProvider theme={theme}>
